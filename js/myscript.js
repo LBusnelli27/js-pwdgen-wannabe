1f5debf115b84ec492d8f98ce922d2d3f3e3a419
Busna27 <lucabusnelli2001@gmail.com>
2022-02-21 15:27:43 +0100

    js + fix & add some things in html and css - done

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,15 @@
 let firstName = prompt("Inserisci il tuo nome");
-console.log(firstName);
+console.log("The first name is: " + firstName);
+document.getElementById('first-name').innerHTML = firstName;
 
 let lastName = prompt("Inserisci il tuo cognome");
-console.log(lastName);
+console.log("The last name is: " + lastName);
+document.getElementById('last-name').innerHTML = lastName;
 
 let favoriteColor = prompt("Inserisci il tuo colore preferito");
-console.log(favoriteColor);
+console.log("The color is: " + favoriteColor);
+document.getElementById('favorite-color').innerHTML = favoriteColor;
 
 let passWord = firstName + lastName + favoriteColor + 21;
-console.log(passWord);
+console.log("The password is: " + passWord);
+document.getElementById('secure-pass').innerHTML = passWord;
