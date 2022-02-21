c55080542be319d4809f8a7453cbdb8d8451195e
Busna27 <lucabusnelli2001@gmail.com>
2022-02-21 15:13:56 +0100

    variable created and display in log

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+let firstName = prompt("Inserisci il tuo nome");
+console.log(firstName);
+
+let lastName = prompt("Inserisci il tuo cognome");
+console.log(lastName);
+
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
+console.log(favoriteColor);
+
+let passWord = firstName + lastName + favoriteColor + 21;
+console.log(passWord);
